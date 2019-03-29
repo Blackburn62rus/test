6bac9a56a8e6c7df84276f2d511208fed9ef6151
CODE <Vadim.17.0@yandex.ru>
2019-03-29 16:24:36 +0300

    Запушил сука2

--- src/Runner.java
+++ src/Runner.java
@@ -5,7 +5,7 @@ public class Runner {
     public static void main(String[] args) {
         DynamicArray<Integer> array = new DynamicArray<>(1, 2, 3, 44, 77);
         DynamicArray<Integer> array1 = new DynamicArray<>(10);
-        DynamicArray<Integer> array2 = new DynamicArray<>();
+        //DynamicArray<Integer> array2 = new DynamicArray<>();
         System.out.println(array1.getCapacity());
         System.out.println(array2.getCapacity());
       //  System.out.println(array);
